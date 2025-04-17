7a1ca5527338cfb4917d2245fd9cd530d72d468b
Erencan10 <131658679+Erencan10@users.noreply.github.com>
2025-04-17 14:40:49 +0200

    Add files via upload

---
+++ css/style.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const questions = document.querySelectorAll(".faq-question");
+    
+    questions.forEach(question => {
+      question.addEventListener("click", ()=> {
+        question.classList.toggle("active");
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector(".icon");
+        
+        if (answer.style.display === "block") {
+          answer.style.display = "none";
+          icon.textContent = "+";
+        } else {
+          answer.style.display = "block";
+          icon.textContent ="-";
+        }
+      });
+    });
+    
+    
+  });
